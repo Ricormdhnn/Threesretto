a1ebd23e259b09f0755439253f24dccbd97e19e0
Ricormdhnn <irzanrico@gmail.com>
2024-09-29 21:27:52 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-// Toggle Class Active
+// Toggle Class Active for Hamburger Menu
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika Hamburger Menu di Klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Click Outside Sidebar to Remove Nav
+// Toggle Class Active for Search Form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Click Outside Element
 const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
